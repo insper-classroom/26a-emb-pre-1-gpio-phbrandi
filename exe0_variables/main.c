7f9955024bfcddcd3bb07bc679a2968ae78ec303
phbrandi <pedrohab1@al.insper.edu.br>
2026-02-23 19:27:14 -0300

    ex variables v2

--- exe0_variables/main.c
+++ exe0_variables/main.c
@@ -2,7 +2,7 @@ void foo(int *a) {
     *a = *a + 1; // acessa ponteiro
 }
 
-void main(void) {
+int main(void) {
     int a = 0;
     int b = 0;
 
@@ -11,6 +11,10 @@ void main(void) {
 
         if (a > 5) {
             b = 1;
+            break;
         }
     }
+    printf("B virou 1\n");
+
+    return 0;
 }
