6280145662d18acad9884671155c25b71f2d52c9
phbrandi <pedrohab1@al.insper.edu.br>
2026-02-23 20:22:45 -0300

    ex5

--- exe5/main.c
+++ exe5/main.c
@@ -22,10 +22,12 @@ int main() {
 
         if (!gpio_get(BTN_PIN)) {
             printf("Botao 1: %d\n", cnt_1++);
+            sleep_ms(100);
         }
 
         if (!gpio_get(BTN_PIN_2)) {
             printf("Botao 2: %d\n", cnt_2++);
+            sleep_ms(100);
         }
     }
 }
